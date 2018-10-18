f6ea8602e502eb5604d2ac365c3841c025797460
Demon Singur <demonsingur@gmail.com>
2018-10-18 10:38:04 +0000

    Remove QTI Perf stack

--- device.mk
+++ device.mk
@@ -350,17 +350,11 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Perf
-TARGET_COMMON_QTI_COMPONENTS := \
-    perf
 
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service \
     android.hardware.power.stats@1.0-service.mock
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
