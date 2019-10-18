b828d9d080043262cc1d208836f901d49acf540c
dianlujitao <dianlujitao@lineageos.org>
2019-10-18 11:05:40 +0800

    whyred: Enable QC framework value-adds
    Change-Id: I1987d3509b3db7cd4204537a6f2dc2ff4c611278

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -219,6 +219,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
 
+# QTI
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
+
+PRODUCT_ODM_PROPERTIES += \
+    ro.vendor.qti.va_odm.support=1
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.ims_volte_enable=1 \
