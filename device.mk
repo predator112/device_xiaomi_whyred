f9b86b45d6c12dac17605e0fb816d94c317286b5
SakilMondal <mondalsakil@gmail.com>
2020-03-27 13:36:54 +0530

    Revert "whyred: Enforce RRO for all resource overlays"
    This reverts commit a56856e832cd691625a84c2aa2c42b9601ce9457.

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/xiaomi/whyred
