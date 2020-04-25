c2c80007f99787e3d997ffadbf2eff233e338de3
Jhonse02 <jhonse021220@gmail.com>
2020-04-24 21:56:37 -0500

    Revert "Update thermal hal to 2.0"
    This reverts commit 0d1f82b52d381fa9eb635ffc597eb06d5d932604.

--- device.mk
+++ device.mk
@@ -447,9 +447,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0 \
-    android.hardware.thermal@2.0-impl \
-    android.hardware.thermal@2.0-service \
+    android.hardware.thermal@1.1 \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
     thermal.sdm660
 
 # Touchscreen
